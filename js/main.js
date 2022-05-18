265f4306d34b6f43706e73945f22beb3382dc526
Pasha <pavel@waff.ly>
2022-05-18 16:03:01 +0300

    change script

--- js/main.js
+++ js/main.js
@@ -35,4 +35,39 @@ document.addEventListener("scroll", () => {
   }
 });
 
+function sendEmail(name, email, number){
+  var templateParams = {
+     name: name,
+     email: email,
+     number: number
+  };
+  emailjs.init("t7eMCLVw9dImSHaoG");
+  emailjs.send('service_bgmxqi9', 'template_z73rd1c', templateParams, 't7eMCLVw9dImSHaoG')
+}
+const form = document.querySelector('.form-section__form')
+const footerForm = document.querySelector('.footer__form');
+const inputs = form.querySelectorAll('input')
+const footerInputs = footerForm.querySelectorAll('input')
+
+form.addEventListener('submit', (e)=> {
+  e.preventDefault()
+  const data = {
+     name: inputs[2].value || 'Невідомий клієнт',
+     email: inputs[0].value || 'Невідомо',
+     number: inputs[1].value
+  }
+  sendEmail(data.name, data.email, data.number)
+  form.reset();
+})
+footerForm.addEventListener('submit', (e)=> {
+  e.preventDefault();
+  const data = {
+     name: 'Невідомий клієнт',
+     email:  'Невідомо',
+     number: footerInputs[0].value
+  }
+  sendEmail(data.name, data.email, data.number)
+  footerForm.reset();
+})
+
 
